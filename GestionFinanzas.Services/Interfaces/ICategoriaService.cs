d98c9e7e5aee71deb9499670dffc4d2d96540962
Dillon Duncan <dillonduncan0607@gmail.com>
2026-09-06 18:43:48 -0500

    Servicios de Categoria

---
+++ GestionFinanzas.Services/Interfaces/ICategoriaService.cs
@@ -0,0 +1,10 @@
+﻿using GestionFinanzas.Data;
+
+namespace GestionFinanzas.Services.Interfaces
+{
+    public interface ICategoriaService
+    {
+        Task<IEnumerable<Categoria>> GetAll();
+        Task<Categoria> Insert(Categoria categoria);
+    }
+}
