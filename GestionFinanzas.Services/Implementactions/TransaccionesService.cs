3adceccc7441a2d61aa25a99c24c8a9fa2af0971
Dillon Duncan <dillonduncan0607@gmail.com>
2026-09-06 20:37:54 -0500

    Servicio de transacciones

---
+++ GestionFinanzas.Services/Implementactions/TransaccionesService.cs
@@ -0,0 +1,58 @@
+﻿using GestionFinanzas.Data;
+using GestionFinanzas.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionFinanzas.Services.Implementactions
+{
+    public class TransaccionesService : ITransaccionesService
+    {
+        private readonly GestionFinanzasContext _context;
+        public TransaccionesService(GestionFinanzasContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Transaccione>> GetAll(int idUsuario)
+        {
+            return await _context.Transacciones
+                .Where(t => t.IdUsuario == idUsuario)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Transaccione>> GetForCategoria(int idUsuario, int idCategoria)
+        {
+            return await _context.Transacciones
+                .Where(t => t.IdUsuario == idUsuario && t.IdCategoria == idCategoria)
+                .ToListAsync();
+        }
+
+        public async Task<Transaccione> GetById(int id, int idUsuario)
+        {
+            return await _context.Transacciones
+                .FirstOrDefaultAsync(t => t.IdTransaccion == id && t.IdUsuario == idUsuario);
+        }
+
+        public async Task<Transaccione> Insert(Transaccione transaccion)
+        {
+            await _context.Transacciones.AddAsync(transaccion);
+            await _context.SaveChangesAsync();
+            return transaccion;
+        }
+
+        public async Task<Transaccione> Update(Transaccione transaccione)
+        {
+            _context.Transacciones.Update(transaccione);
+            await _context.SaveChangesAsync();
+            return transaccione;
+        }
+
+        public async Task<bool> Delete(int idTransaccion)
+        {
+            var transaccion = await _context.Transacciones.FindAsync(idTransaccion);
+            if (transaccion == null) return false;
+            transaccion.EstadoActivoTransaccion = false;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ GestionFinanzas.Services/Interfaces/ITransaccionesService.cs
@@ -0,0 +1,14 @@
+﻿using GestionFinanzas.Data;
+
+namespace GestionFinanzas.Services.Interfaces
+{
+    public interface ITransaccionesService
+    {
+        Task<IEnumerable<Transaccione>> GetAll(int idUsuario);
+        Task<IEnumerable<Transaccione>> GetForCategoria(int idUsuario, int idCategoria);
+        Task<Transaccione> GetById(int id, int idUsuario);
+        Task<Transaccione> Insert(Transaccione transaccion);
+        Task<Transaccione> Update(Transaccione transaccione);
+        Task<bool> Delete(int idTransaccion);
+    }
+}
