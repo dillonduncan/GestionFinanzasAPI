22093cc687c757afbffba84b31eec3c79d232d04
Dillon Duncan <dillonduncan0607@gmail.com>
2026-09-06 20:25:32 -0500

    servicio de meta

---
+++ GestionFinanzas.Services/Implementactions/MetaService.cs
@@ -0,0 +1,48 @@
+﻿using GestionFinanzas.Data;
+using GestionFinanzas.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionFinanzas.Services.Implementactions
+{
+    public class MetaService : IMetaService
+    {
+        private readonly GestionFinanzasContext _context;
+        public MetaService(GestionFinanzasContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Meta>> GetAll(int idUsuario)
+        {
+            return await _context.Metas.Where(m => m.IdUsuario == idUsuario).ToListAsync();
+        }
+
+        public async Task<Meta> GetById(int id, int idUsuario)
+        {
+            return await _context.Metas.FirstOrDefaultAsync(m => m.IdMeta == id && m.IdUsuario == idUsuario);
+        }
+
+        public async Task<Meta> Insert(Meta meta)
+        {
+            await _context.Metas.AddAsync(meta);
+            await _context.SaveChangesAsync();
+            return meta;
+        }
+
+        public async Task<Meta> Update(Meta meta)
+        {
+            _context.Metas.Update(meta);
+            await _context.SaveChangesAsync();
+            return meta;
+        }
+
+        public async Task<bool> Delete(int idMeta)
+        {
+            var meta = await _context.Metas.FindAsync(idMeta);
+            if (meta == null) return false;
+            meta.EstadoActivoMeta = false;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ GestionFinanzas.Services/Interfaces/IMetaService.cs
@@ -0,0 +1,13 @@
+﻿using GestionFinanzas.Data;
+
+namespace GestionFinanzas.Services.Interfaces
+{
+    public interface IMetaService
+    {
+        Task<IEnumerable<Meta>> GetAll(int idUsuario);
+        Task<Meta> GetById(int id, int idUsuario);
+        Task<Meta> Insert(Meta meta);
+        Task<Meta> Update(Meta meta);
+        Task<bool> Delete(int idMeta);
+    }
+}
