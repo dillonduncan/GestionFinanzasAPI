959e6509c1dbaf04dc6c374295cf7cd49e94f617
Dillon Duncan <dillonduncan0607@gmail.com>
2026-09-08 15:02:36 -0500

    configuracion del program

--- GestionFinanzasAPI/Program.cs
+++ GestionFinanzasAPI/Program.cs
@@ -1,14 +1,24 @@
+using GestionFinanzas.Data;
+using GestionFinanzas.Services.Implementations;
+using GestionFinanzas.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<GestionFinanzasContext>(opc => opc.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IMetaService, MetaService>();
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<ITransaccionesService, TransaccionesService>();
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.     
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
