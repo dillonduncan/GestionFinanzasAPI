ee59ac6a37f7b1a4c3ea996476017d626406a223
Dillon Duncan <dillonduncan0607@gmail.com>
2026-09-18 19:55:58 -0500

    usuarios DTOs

---
+++ GestionFinanzasAPI/DTOs/Requests/UsuarioCreateDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionFinanzasAPI.DTOs.Requests
+{
+    public class UsuarioCreateDTO
+    {
+        [Required(ErrorMessage = "El numero de identificacion es obligatorio.")]
+        public string NumeroIdentificacion { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string NombreUsuario { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        public string ApellidoUsuario { get; set; }
+
+        [Required, EmailAddress]
+        public string CorreoUsuario { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string ContraseñaUsuario { get; set; }
+    }
+}
---
+++ GestionFinanzasAPI/DTOs/Responses/UsuarioResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace GestionFinanzasAPI.DTOs.Responses
+{
+    public class UsuarioResponseDTO
+    {
+        public int IdUsuario { get; set; }
+        public string NumeroIdentificacion { get; set; }
+        public string NombreUsuario { get; set; }
+        public string ApellidoUsuario { get; set; }
+        public string CorreoUsuario { get; set; }
+    }
+}
